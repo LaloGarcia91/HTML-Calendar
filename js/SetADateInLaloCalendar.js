b8bf4ec3c67014827cb18b76ceb8fc82fb92e9c9
Eduardo Garcia G <lalo.garcia.g91@gmail.com>
2020-05-21 02:02:08 -0500

    refactored.

--- js/SetADateInLaloCalendar.js
+++ js/SetADateInLaloCalendar.js
@@ -1,17 +1,28 @@
 
-function SetADateInLaloCalendar(selectorReferenceString, monthDay, monthNumber, year){
+function SetADateInLaloCalendar(selectorReference, monthDay, monthNumber, year){
     var _AlterCalendarGUI = AlterCalendarGUI();
     var _CalendarSelectors = CalendarSelectors();
-    
-    var selectorReference;
+        
+
     try {
-        selectorReference = document.querySelector(selectorReferenceString);
-        var calendarLoader = _CalendarSelectors.GetCalendarLoader(selectorReference);
+        var selectorReferenceType = typeof selectorReference;
+        var selector;
+        switch(selectorReferenceType){
+            case 'string':
+                selector = document.querySelector(selectorReference);
+                break;
+
+            default:
+                selector = selectorReference;
+                break;
+        }
+        
+        var calendarLoader = _CalendarSelectors.GetCalendarLoader(selector);
         _AlterCalendarGUI.SetNewDateInCalendarAndSelectorDisplay(calendarLoader, monthDay, (monthNumber-1), year);
     }catch(err){
         // the selector does in the DOM
         console.log("The selector:");
-        console.log(selectorReferenceString);
+        console.log(selectorReference);
         console.log("does not exist in the DOM.");
         return false;
     }
